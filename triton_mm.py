647105958ca047b5ba23bdec0ba97ebd92abdfa4
egorsmth <mattspring0@gmail.com>
2026-05-30 14:04:10 +0300

    matmul

--- triton_mm.py
+++ triton_mm.py
@@ -12,7 +12,7 @@ def triton_matmul_naive(A, B, C, M, N, K):
 
     if i >= M or j >= N:
         return
-    c_val = tl.zeros((), dtype=tl.float16)
+    c_val = tl.zeros((), dtype=tl.float32)
 
     for k in range(K):
         a_val = tl.load(A + i * K + k)
@@ -53,29 +53,76 @@ def triton_matmul_grouped(A, B, C, M: tl.constexpr, N: tl.constexpr, K: tl.const
 
 
 def run():
-    M, N, K = 16, 16, 16
+    M, N, K = 512, 512, 512
 
     a = torch.randn(M, K, dtype=torch.float16, device=DEVICE)
     b = torch.randn(K, N, dtype=torch.float16, device=DEVICE)
     o = torch.zeros((M, N), dtype=torch.float16, device=DEVICE)
 
-    print(a)
-    print(b)
-    print(torch.matmul(a, b))
-   # grid = (16 * 16,)
+    perfect = torch.matmul(a, b)
 
-    #triton_matmul_naive[grid](a, b, o, M, N, K)
-   # print(o)
-   # o = torch.zeros((M, N), dtype=torch.float16, device=DEVICE)
-   # triton_matmul_rowed[grid](a, b, o, M, N, K)
-   # print(o)
+    grid = (M * N,)
+
+    triton_matmul_naive[grid](a, b, o, M, N, K)
+    if torch.allclose(perfect, o, atol=0.125, rtol=0):
+        print("✅ matmul naive and Torch match")
+    else:
+        print(perfect[:5, :5])
+        print(o[:5, :5])
+    o = torch.zeros((M, N), dtype=torch.float16, device=DEVICE)
+    triton_matmul_rowed[grid](a, b, o, M, N, K)
+    if torch.allclose(perfect, o, atol=0.125, rtol=0):
+        print("✅ matmul rowed and Torch match")
     
     o = torch.zeros((M, N), dtype=torch.float16, device=DEVICE)
-    GROUP_SIZE = 2
-    triton_matmul_grouped[(8, 8)](a, b, o, M, N, K, GROUP_SIZE)
-    print(o)
+    GROUP_SIZE = 8
+    triton_matmul_grouped[(M // GROUP_SIZE, N // GROUP_SIZE)](a, b, o, M, N, K, GROUP_SIZE)
+    if torch.allclose(perfect, o, atol=0.125, rtol=0):
+        print("✅ matmul grouped and Torch match")
 
 
 
 run()
 
+
+
+configs = []
+for i in [2, 8, 16]:
+    configs.append(
+            triton.testing.Benchmark(
+                x_names=["M", "N", "K"],
+                x_vals=[128 * i for i in [2, 4, 8]],
+                line_arg="provider",
+                line_vals=['naive', 'rowed', 'grouped'],
+                line_names=['naive', 'rowed', 'grouped'],
+                ylabel='TFLOPS',
+                plot_name=f'matmul-perf-gr{i}-no-torch',
+                args={"GROUP_SIZE": i},
+                )
+            )
+
+@triton.testing.perf_report(configs)
+def benchmark(M, N, K, GROUP_SIZE, provider):
+
+    a = torch.randn(M, K, dtype=torch.float16, device=DEVICE)
+    b = torch.randn(K, N, dtype=torch.float16, device=DEVICE)
+    o = torch.zeros((M, N), dtype=torch.float16, device=DEVICE)
+
+    rep = 10
+    qs = [0.5, 0.2, 0.8]
+    if provider == 'torch':
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: torch.matmul(a, b), quantiles=qs, rep=rep)
+    if provider == 'naive':
+        grid = (M * N,)
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: triton_matmul_naive[grid](a, b, o, M, N, K), quantiles=qs, rep=rep)
+    if provider == 'rowed':
+        grid = (M * N,)
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: triton_matmul_rowed[grid](a, b, o, M, N, K), quantiles=qs, rep=rep)
+    if provider == 'grouped':
+        grid = (M // GROUP_SIZE, N // GROUP_SIZE)
+        print(grid)
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: triton_matmul_grouped[grid](a, b, o, M, N, K, GROUP_SIZE), quantiles=qs, rep=rep)
+    perf = lambda ms: 2 * M * N * K * 1e-12 / (ms * 1e-3)
+    return perf(ms), perf(max_ms), perf(min_ms)
+
+benchmark.run(save_path='./mm', show_plots=True, print_data=True)
